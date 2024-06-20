0c6dd9d1be7988af851a1eae7e41bef6ce63a714
Jose Pedraza <jpedraza066@gmail.com>
2024-06-20 14:11:10 -0500

    Arm Servo V1

---
+++ arm_servos/arm_servos.ino
@@ -0,0 +1,59 @@
+#include <Servo.h>
+
+Servo LeftEyeServo;
+Servo RightEyeServo;
+Servo WristServo;
+Servo ArmServo;
+
+const int LeftEyeServoPin = 0;
+const int RightEyeServoPin = 0;
+const int WristServoPin = 10;
+const int ArmServoPin = 9;
+
+void setup() {
+  Serial.begin(9600);
+  ArmServo.attach(ArmServoPin);
+  WristServo.attach(WristServoPin);
+  Serial.println("--------------------");
+  Serial.println("Servos Attached");
+  
+  delay(500);
+
+  ArmServo.write(90);
+  WristServo.write(150);
+  Serial.println("Servos Calibrated");
+
+  delay(2000);
+
+  ArmServo.write(90+70);
+  Serial.println("Arm Moved Up");
+
+  delay(1500);
+
+  WristServo.write(90-45);
+  Serial.println("Wrist Turn Down");
+
+  delay(750);
+
+  WristServo.write(150);
+  Serial.println("Wrist Turn Up");
+
+  delay(750);
+
+  WristServo.write(90-45);
+  Serial.println("Wrist Turn Down");
+  
+  delay(750);
+
+  WristServo.write(150);
+  Serial.println("Wrist Turn Up");
+
+  delay(2000); 
+
+  ArmServo.write(90);
+  Serial.println("Arm Moved Down");
+}
+
+void loop() {
+
+}
